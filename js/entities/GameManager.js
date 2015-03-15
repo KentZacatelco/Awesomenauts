09a0b1b942ac1c0542eaef05bc5c8bf84a547a03
Kent Zacatelco <kentzacattelco05@gmail.com>
2015-03-15 16:24:53 -0700

    .

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -16,7 +16,7 @@ game.GameTimerManager = Object.extend({
     
     goldTimerCheck: function(){
         if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
-            game.data.gold += 1;
+            game.data.gold += (game.data.exp1 + 1);
         }
     },
     
@@ -47,15 +47,69 @@ game.HeroDeathManager = Object.extend({
 game.ExperienceManager = Object.extend({
     init: function(x, y, settings){
         this.alwaysUpdate = true;
+        this.gameover = false;
     },
     
     update: function(){
-        if(game.data.win === true){
-            game.data.exp += 10;
-        }else if(game.data.win === false){
-            game.data.exp =+ 1;
+        if(game.data.win === true && !this.gameover){
+            this.gameOver(true);
+        }else if(game.data.win === false && !this.gameover){
+            this.gameOver(false);
         }
         
         return true;
+    },
+    
+    gameOver: function(win){
+        if(win){
+            game.data.exp += 10;
+        }else{
+            game.data.exp =+ 1;
+        }
+        this.gameover = true;
+        me.save.exp = game.data.exp;
+    }
+});
+
+game.SpendGold = Object.extend({
+    init: function(x, y, settings){
+        this.now = new Date().getTime();
+        this.lastBuy = new Date().getTime();
+        this.paused = false;
+        this.alwaysUpdate = true;
+        this.updateWhenPaused = true;
+        this.buying = false;
+    },
+    
+    update: function(){
+        this.now = new Date().getTime();
+        
+        if(me.input.isKeyPressed("buy") && this.now-this.lastBuy >=1000){
+            this.lastBuy = this.now;
+            if(!this.buying){
+                this.startBuying();
+            }else{
+                this.stopBuying();
+            }
+        }
+        
+        return true;
+    },
+    startBuying: function(){
+        this.buying = true;
+        me.state.pause(me.state.PLAY);
+        game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+        game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+        game.data.buyscreen.updataWhenPaused = true;
+        game.data.buyscreen.setOpacity(0.8);
+        me.game.world.addChild(game.data.buyscreen, 34);
+        game.data.player.body.setVelocity(0,0);
+    },
+    
+    stopBuying: function(){
+        this.buying = false;
+        me.state.resume(me.state.PLAY);
+        game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+        me.game.world.removeChild(game.data.buyscreen);
     }
 });
